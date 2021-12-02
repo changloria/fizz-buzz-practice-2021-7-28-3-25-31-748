bbb61244523dc07d193939f8ee25821f58bc13d5
Gloria Chan <changl3@oocl.com>
2021-12-02 13:56:39 +0800

    [java-TDD-FizzBuzz][Gloria] -feat: init testcase commit

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,4 +1,10 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+    public static void main(String[] args) {
+
+    }
+    public String countOff(int order) {
+        return null;
+    }
 }
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,17 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    void should_return_order_when_countoff_given_order_number_with_normal_case() {
+        //given
+        int order = 1;
+        //when
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String result = fizzBuzz.countOff(order);
+        //then
+        assertEquals("1", result);
+    }
 }
